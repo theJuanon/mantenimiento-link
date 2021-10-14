1045c24c6d05b2039e41f76c338ed4f1c4d83c90
Juan Hernández <56099545+theJuanon@users.noreply.github.com>
2021-10-14 11:14:42 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Ejercicio - Link
+
+Proyecto Java para prácticas de depuración e Ingeniería Inversa
+
+* Ejecute el programa con el comando:
+```
+gradle run
+```
+
+Este repositorio no incluye pruebas unitarias ni está habilitado para Autograding
---
+++ src/main/java/wsLink/PruebaDebugger.java
@@ -0,0 +1,9 @@
+package wsLink;
+
+public class PruebaDebugger {
+	public static void main(String[] args) {
+        ClaseParaDepurar clase = new ClaseParaDepurar();
+        double valor = clase.echaCuentas();
+        System.out.println("Sale "+valor);
+    }
+}
